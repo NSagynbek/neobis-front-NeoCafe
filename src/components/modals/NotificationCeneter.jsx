5693f833c419d21757441543ca2c66b0c7befeb8
nursultan1196 <nursultan1196@gmail.com>
2024-02-01 17:42:24 +0600

    check for changes in GitLab

--- src/components/modals/NotificationCeneter.jsx
+++ src/components/modals/NotificationCeneter.jsx
@@ -1,5 +1,5 @@
 import  { useState, useEffect } from 'react';
-import WebSocket from 'react-websocket'
+import WebSocket from 'react-websocket';
 import "./notificationCenter.css"
 
 const NotificationCenter = () => {
