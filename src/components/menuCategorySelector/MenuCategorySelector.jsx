b642a7eae1c451155588852140df23e0c521ba1d
nursultan1196 <nursultan1196@gmail.com>
2024-02-22 19:11:20 +0600

    TT2-40<Remove menu category code to a seperate MenuCategorySelector component>

---
+++ src/components/menuCategorySelector/MenuCategorySelector.jsx
@@ -0,0 +1,80 @@
+import "./menuCategorySelector.css";
+import { InputAdornment, IconButton } from "@mui/material";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { useState } from "react";
+function MenuCategorySelector() {
+
+    const [category,setCategory] = useState(null);
+    const [isClicked,setIsClicked] = useState(false);
+
+    const handleCategory = (category)=>{      
+        setCategory(category)
+        setIsClicked(!isClicked);
+    }
+
+    const handleToggle = () => {
+        setIsClicked(!isClicked);
+    };
+    
+    return (
+      <div 
+        className="add-menu-new-item-category-subContainer-container">
+        <p className="item-name__label">Категория</p>
+        <div className="add-menu-new-item-dropdown-container">
+          <div 
+            className={`add-menu-new-item-dropdown-subContainer 
+                      ${isClicked ? "transform" : ""}`}>
+            <p className="add-menu-new-item-dropdown-title">
+              {category ? category[1] : "Выберите категорию"}
+            </p>
+            <InputAdornment 
+              position="end" 
+              className="add-menu-new-item-dropdown-icons" 
+              onClick={handleToggle}>
+              <IconButton>
+                {isClicked ? (
+                  <KeyboardArrowUpIcon style={{ color: "#5B7E9A" }} />
+                ) : (
+                  <KeyboardArrowDownIcon style={{ color: "#5B7E9A" }} />
+                )}
+              </IconButton>
+            </InputAdornment>
+          </div>
+          <ul 
+            className=
+              {`add-menu-new-item-dropdown-categries 
+              ${isClicked ? "add-menu-new-item-dropdown-toggle" : ""}`}>
+            <li 
+              className="add-menu-new-item-dropdown-item" 
+              onClick={() => handleCategory(["coffee", "Кофе"])}>
+              Кофе
+            </li>
+            <li 
+              className="add-menu-new-item-dropdown-item" 
+              onClick={() => handleCategory(["desserts", "Десерты"])}>
+              Десерты
+            </li>
+            <li 
+              className="add-menu-new-item-dropdown-item" 
+              onClick={() => handleCategory(["bakery", "Выпечка"])}>
+              Выпечка
+            </li>
+            <li 
+              className="add-menu-new-item-dropdown-item" 
+              onClick={() => handleCategory(["coktail", "Коктейли"])}>
+              Коктейли
+            </li>
+            <li 
+              className="add-menu-new-item-dropdown-item" 
+              onClick={() => handleCategory(["tea", "Чай"])}>
+              Чай
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+  export default MenuCategorySelector;
+  
--- src/modalwindows/addNewMenuItem/AddNewMenuItem.jsx
+++ src/modalwindows/addNewMenuItem/AddNewMenuItem.jsx
@@ -5,257 +5,177 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { closeModal } from "../../redux";
 import { cloudUpload } from "../../assets/index";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { useSelector } from "react-redux";
 import Ingredients from "../../components/ingredients/Ingredients";
+import MenuCategorySelector from "../../components/menuCategorySelector/MenuCategorySelector";
 
 function AddNewMenuItem() {
-    const modalData = useSelector((state)=>state.modalData);
+  const modalData = useSelector((state) => state.modalData);
+  const [activeSection, setActiveSection] = useState(null);
+  const [files, setFiles] = useState(null);
+  const [isImageDrag, setIsImageDrag] = useState(false);
+  const [ingredientsList, setIngredientsList] = useState([]);
 
-    const [activeSection, setActiveSection] = useState(null);
-    const[files,setFiles] = useState(null);
-    const [isClicked,setIsClicked] = useState(false);
-    const [category,setCategory] = useState(null);
-    const [isImageDrag,setIsImageDrag]=useState(false);
-    const [ingredientsList, setIngredientsList] = useState([]);
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const handleButtonClick = (section) => {
+    setActiveSection(section === activeSection ? null : section);
+  };
 
-    const handleButtonClick = (section) => {
-        setActiveSection(section === activeSection ? null : section);
-    };
+  const isActive = (section) => section === activeSection;
 
-    const isActive = (section) => section === activeSection;
+  const handleCloseModal = () => {
+    dispatch(closeModal());
+  };
 
-    const handleCloseModal = () => {
-        dispatch(closeModal());
-    }
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setIsImageDrag(true);
+  };
 
-    const handleDragOver = (e) => {
-        e.preventDefault();
-        setIsImageDrag(true);
-    };
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setFiles(e.dataTransfer.files[0]);
+    setIsImageDrag(false);
+  };
 
-    const handleDrop = (e) => {
-        e.preventDefault();
-        setFiles(e.dataTransfer.files[0]);
-        setIsImageDrag(false);
-      
-    };
+  const handleFileInputChange = async (e) => {
+    setFiles(e.target.files[0]);
+  };
 
-    const handleFileInputChange = async (e) => {
-        setFiles(e.target.files[0])
-    };
+  const addIngredients = () => {
+    setIngredientsList(
+      [...ingredientsList, 
+      <Ingredients key={ingredientsList.length} />
+    ]);
+  };
 
-    const handleToggle = () => {
-        setIsClicked(!isClicked);
-    };
-    
-    const handleCategory = (category)=>{      
-        setCategory(category)
-        setIsClicked(!isClicked);
-    }
+  return (
+    <div className="add-menu-new-item-container">
+      <div className="menu-add-new-item-header">
+        {modalData.type === "createMenu" && 
+        <p className="menu-add-new-item-title">Новая позиция</p>}
+        {modalData.type === "editMenu" && 
+        <p className="menu-add-new-item-title">Редактирование</p>}
+        <InputAdornment 
+          position="end" 
+          className="menu-category-add-close-icon" 
+          onClick={handleCloseModal}>
+          <IconButton>
+            <CloseIcon style={{ color: "#2A3440" }} />
+          </IconButton>
+        </InputAdornment>
+      </div>
 
-   
-
-    const addIngredients = () => {
-      setIngredientsList([...ingredientsList, 
-        <Ingredients key={ingredientsList.length} />
-      ]);
-    };
-
-    return (
-        <div className="add-menu-new-item-container">
-            <div className="menu-add-new-item-header">
-              {modalData.type==="createMenu"&&<p className="menu-add-new-item-title">Новая позиция</p>}
-              {modalData.type==="editMenu"&&<p className="menu-add-new-item-title">Редактирование</p>}
-                
-                <InputAdornment
-                    position="end"
-                    className="menu-category-add-close-icon"
-                    onClick={handleCloseModal}
-                >
-                    <IconButton>
-                        <CloseIcon style={{ color: "#2A3440" }} />
-                    </IconButton>
-                </InputAdornment>
+      <div 
+        className="menu-add-new-item-image-container" 
+        onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleDrop(e)}>
+        <div 
+          className={`menu-add-new-item-image-dashed 
+          ${isImageDrag ? "image-upload-active" : ""}`}>
+          <p className="menu-add-new-item-image-title">
+            Добавьте фото к позиции
+          </p>
+          <label htmlFor="menu-add-new-item-image">
+            <div className="menu-add-new-item-image-subContainer">
+              <img 
+                src={files ? URL.createObjectURL(files) : 
+                   (cloudUpload)} alt="cloud upload image" 
+              />
             </div>
+            <p className="menu-add-new-item-image-text">
+              Перетащите изображение для изменения или <span>обзора</span>
+            </p>
+          </label>
+          <input 
+            id="menu-add-new-item-image" 
+            type="file" 
+            onChange={handleFileInputChange} />
+        </div>
+      </div>
 
-            <div className="menu-add-new-item-image-container"
-              onDragOver={(e)=>handleDragOver(e)}
-              onDrop={(e)=>handleDrop(e)}
-            >
-               
-              <div className={`menu-add-new-item-image-dashed 
-                ${isImageDrag ? "image-upload-active" : ""}`}>          
-                <p
-                    className="menu-add-new-item-image-title"
-                >
-                    Добавьте фото к позиции
-                </p>
-                <label htmlFor="menu-add-new-item-image">
-                    <div 
-                      className="menu-add-new-item-image-subContainer"
-                      
-                    >        
-                      <img src={files ? URL.createObjectURL(files):
-                         (cloudUpload) } alt="cloud upload image"/>           
-                    </div>
-                    <p
-                        className="menu-add-new-item-image-text"
-                    >
-                        Перетащите изображение для изменения или
-                        <span> обзора</span>
-                    </p>
-                </label>
-                <input
-                    id="menu-add-new-item-image"
-                    type="file"
-                    onChange={handleFileInputChange}
-                />
-            </div>             
+      <div className="add-menu-new-item__name_price-container">
+        <p 
+          className="add-menu-new-item__name_price-text">
+          Наименование, категория и стоимость
+        </p>
+        <label 
+          htmlFor="item-name" 
+          className="item-name__label">
+          Наименование
+        </label>
+        <input id="item-name" type="text" />
+        <div className="text-text">
+          <label 
+            htmlFor="item-description" 
+            className="item-name__label">
+            Описание
+          </label>
+          <textarea 
+            id="item-description" 
+            name="myTextArea" 
+            rows="1" 
+            cols="50">
+          </textarea>
+        </div>
+        <div className="add-menu-new-item-content-container">
+          <div className="add-menu-new-item-category-container">
+            <MenuCategorySelector />
+            <div className="add-menu-new-item-price-container">
+              <label 
+                htmlFor="add-menu-new-item-price-item" 
+                className="add-menu-new-item-price-item">
+                Стоимость
+              </label>
+              <input 
+                id="add-menu-new-item-price-item" 
+                type="text" 
+              />
+            </div>
+          </div>
+          <div className="add-menu-new-ingredients-container">
+            <p 
+              className="add-menu-new-item__name_price-text">
+              Состав блюда и граммовка
+            </p>
+            <Ingredients />
+            {ingredientsList.map((ingredient, index) => (
+              <div key={index}>{ingredient}</div>
+            ))}
           </div>
 
-            <div className="add-menu-new-item__name_price-container">
-                <p
-                    className="add-menu-new-item__name_price-text"
-                >
-                    Наименование, категория и стоимость
-                </p>
-                <label
-                    htmlFor="item-name"
-                    className="item-name__label"
-                >
-                    Наименование
-                </label>
-                <input id="item-name" type="text" />
-                <div className="text-text">
-                <label
-                    htmlFor="item-description"
-                    className="item-name__label"
-                >
-                    Описание
-                </label>
-                <textarea 
-                  id="item-description" 
-                  name="myTextArea" 
-                  rows="1" cols="50">
-                </textarea>
-                </div>
-                
-                <div className="add-menu-new-item-content-container">
-                    <div className="add-menu-new-item-category-container">
-                        <div className="add-menu-new-item-category-subContainer-container">
-                            <p className="item-name__label">Категория</p>
-                            <div className="add-menu-new-item-dropdown-container">
-                                <div 
-                                  className={`add-menu-new-item-dropdown-subContainer 
-                                  ${isClicked?("transform") : 
-                                  ("")}`}  
-                                >
-                                    <p
-                                        className="add-menu-new-item-dropdown-title">
-                                        {category?category[1]:"Выберите категорию"}
-                                    </p>
-                                    <InputAdornment
-                                        position="end"
-                                        className="add-menu-new-item-dropdown-icons"
-                                        onClick={handleToggle} 
-                                    >
-                                        <IconButton>
-                                            {isClicked ?
-                                              (<KeyboardArrowUpIcon style={{ color: "#5B7E9A" }} />):                          
-                                              (<KeyboardArrowDownIcon style={{ color: "#5B7E9A" }} />)
-                                            }
-                                        </IconButton>
-                                    </InputAdornment>
-                                </div>
-                                <ul className={`add-menu-new-item-dropdown-categries 
-                                             ${isClicked?"add-menu-new-item-dropdown-toggle":""}`}>
-                                    <li 
-                                      className="add-menu-new-item-dropdown-item"
-                                      onClick={()=>handleCategory(["coffee","Кофе"])}
-                                    >
-                                      Кофе
-                                    </li>
-                                    <li 
-                                      className="add-menu-new-item-dropdown-item"
-                                      onClick={()=>handleCategory(["desserts","Десерты"])}
-                                    >
-                                      Десерты
-                                    </li>
-                                    <li 
-                                      className="add-menu-new-item-dropdown-item"
-                                      onClick={()=>handleCategory(["bakery","Выпечка"])}
-                                    >
-                                      Выпечка
-                                    </li>
-                                    <li 
-                                      className="add-menu-new-item-dropdown-item"
-                                      onClick={()=>handleCategory(["coktail","Коктейли"])}
-                                    >
-                                      Коктейли
-                                    </li>
-                                    <li 
-                                      className="add-menu-new-item-dropdown-item"
-                                      onClick={()=>handleCategory(["tea","Чай"])}
-                                    >
-                                      Чай
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="add-menu-new-item-price-container">
-                            <label
-                                htmlFor="add-menu-new-item-price-item"
-                                className="add-menu-new-item-price-item"
-                            >
-                                Стоимость
-                            </label>
-                            <input id="add-menu-new-item-price-item" type="text" />
-                        </div>
-                    </div>
-                    <div className="add-menu-new-ingredients-container">
-                        <p className="add-menu-new-item__name_price-text">
-                          Состав блюда и граммовка
-                        </p>
-                        {ingredientsList.map((ingredient, index) => (
-                        <div key={index}>{ingredient}</div>
-                         ))}                                      
-                    </div>
-                 
-                    <div className="test-btn">
-                      <button 
-                        className="add-more-menu-items-btn"
-                        onClick={addIngredients}
-                      >
-                        Добавить еще +
-                      </button>
-                    </div>
-                    <div className="menu-add-new-item-btns-container">
-                      <button
-                        className={`new-menu-category-modal__cancel-button 
-                        ${isActive("cancel") ? "menu-category-btn-active" : ""}`}
-                        onClick={() => handleButtonClick("cancel")}
-                      >
-                        Сохранить
-                      </button>
-                      <button
-                        className={`new-menu-category-modal__add-button 
-                        ${isActive("add") ? "menu-category-btn-active" : ""}`}
-                        onClick={() => {
-                        handleButtonClick("add")
-                        handleCloseModal()
-                        }}
-                      >
-                        Отмена
-                      </button>
-                    </div>
-                </div>
-            </div>          
+          <div className="test-btn">
+            <button 
+              className="add-more-menu-items-btn" 
+              onClick={addIngredients}>
+              Добавить еще +
+            </button>
+          </div>
+
+          <div className="menu-add-new-item-btns-container">
+            <button
+              className={`new-menu-category-modal__cancel-button 
+              ${isActive("cancel") ? "menu-category-btn-active" : ""}`}
+              onClick={() => handleButtonClick("cancel")}
+            >
+              Сохранить
+            </button>
+            <button
+              className={`new-menu-category-modal__add-button 
+              ${isActive("add") ? "menu-category-btn-active" : ""}`}
+              onClick={() => {
+                handleButtonClick("add");
+                handleCloseModal();
+              }}
+            >
+              Отмена
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
+
 
 export default AddNewMenuItem;
